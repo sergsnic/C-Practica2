ad5b700269d524598b7f007abe7c1f136950126c
Sergey <sergsnic@mail.ru>
2022-08-09 14:31:15 +0300

    add C# Seminar2 Task(1-3).

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число: ");
+string numeric = Console.ReadLine();
+if(numeric.Length < 3) Console.WriteLine($"третьей цифры числа {numeric} нет.");
+else Console.WriteLine($"Третья цифра числа {numeric} равна: {numeric[2]}");
---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Введите число от (1-7): ");
+string numeric = Console.ReadLine();
+switch (numeric)
+{
+    case "1": 
+        Console.WriteLine($"{numeric} нет, Рабочий день, понедельник.");
+        break;
+    case "2":
+        Console.WriteLine($"{numeric} нет, Рабочий день, вторник.");
+        break;
+    case "3":
+        Console.WriteLine($"{numeric} нет, Рабочий день, среда.");
+        break;
+    case "4":
+        Console.WriteLine($"{numeric} нет, Рабочий день, четверг.");
+        break;
+    case "5":
+        Console.WriteLine($"{numeric} нет, Рабочий день, пятница.");
+        break;
+    case "6":
+        Console.WriteLine($"{numeric} да, Выходной день, суббота.");
+        break;
+    case "7":
+        Console.WriteLine($"{numeric} да, Выходной день, воскресенье.");
+        break;
+}
